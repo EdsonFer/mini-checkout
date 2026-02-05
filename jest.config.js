82bbed89c7a8bcc852f8a58c1c156b2e04174d6c
Edson <edlivelifehd1z@gmail.com>
2026-02-05 01:18:33 -0300

    chore: initial project setup

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: [
+    'features/**/domain/**/*.ts',
+    '!features/**/__tests__/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+}
+
+module.exports = config
